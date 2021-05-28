9cb501bb20a1ac62dda9f1da425fc38e687d8f87
Caleb Short <calebshort4697@gmail.com>
2021-05-28 11:35:30 -0400

    Created new skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/user', require('./user'));
-router.use('/model', require('./model'));
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('not implemented');
 });
-
 
 module.exports = router;
