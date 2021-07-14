6ca1a37746042fa31b569dbe6f496ff0c4d0579a
Caleb Short <calebshort4697@gmail.com>
2021-07-14 09:35:29 -0700

    Fixed small issue with async jest unit tests

--- models/template_field.unit.test.js
+++ models/template_field.unit.test.js
@@ -4,21 +4,23 @@ const Util = require('../lib/util');
 jest.mock('MongoDB');
 
 // TODO: After adding the describe blocks, there is an error. Figure this out.
+// TODO: add the next test using mocks. Reference https://stackoverflow.com/questions/43265944/is-there-any-way-to-mock-private-functions-with-jest
+
 describe('validateAndCreateOrUpdateField', () => {
   describe('common between create and update', () => {
-    test('field must be an object', () => {
+    test('field must be an object', async() => {
       await expect(TF.validateAndCreateOrUpdateField(null, null, null)).rejects.toThrowError(Util.InputError);
     });
   });
 
   describe('update', () => {
-    test('provided uuid and field uuid must match', () => {
+    test('provided uuid and field uuid must match', async () => {
       await expect(TF.validateAndCreateOrUpdateField({uuid: "a"}, null, "b")).rejects.toThrowError(Util.InputError);
     });
-    test('field uuid is valid', () => {
+    test('field uuid is valid', async () => {
       await expect(TF.validateAndCreateOrUpdateField({uuid: 5}, null, 5)).rejects.toThrowError(Util.InputError);
     });
-    test('field uuid is valid', () => {
+    test('field uuid is valid', async () => {
       await expect(TF.validateAndCreateOrUpdateField({uuid: "34"}, null, "34")).rejects.toThrowError(Util.InputError);
     });
 
