94580a7b34b1429b141c355a6b1a62b8b65c0abd
Caleb Short <calebshort4697@gmail.com>
2021-07-22 12:24:19 -0700

    Finished integration tests for template endpoints, at least for this phase.

--- integration_tests/template.int.test.js
+++ integration_tests/template.int.test.js
@@ -25,7 +25,7 @@ async function clearDatabase() {
   await db.collection('template_fields').deleteMany();
 }
 
-afterEach(async() => {
+beforeEach(async() => {
   await clearDatabase();
 });
 
@@ -52,7 +52,7 @@ const createSuccessTest = async (data) => {
   return response.body.uuid;
 };
 
-describe("create / get draft", () => {
+describe("create (and get draft after a create)", () => {
 
   describe("Success cases", () => {
 
@@ -68,8 +68,18 @@ describe("create / get draft", () => {
       await createSuccessTest(data);
     });
   
+    // TODO: when field endpoints are added, test they are also created
     test("Create template with related template and field", async () => {
   
+      let related_template_data = { 
+        "name": "create template child",
+        "description": "the child of create template",
+        "fields": [
+          { 
+            "name": "create template child field"
+          }
+        ]
+      };
       let data = { 
         "name": "create template",
         "description": "a template to test a create",
@@ -79,19 +89,20 @@ describe("create / get draft", () => {
             "description": "a dummy field to go with a template create"
           }
         ],
-        "related_templates": [
-          { 
-            "name": "create template child",
-            "description": "the child of create template",
-            "fields": [
-              { 
-                "name": "create template child field"
-              }
-            ]
-          }
-        ]
+        "related_templates": [related_template_data]
       };
-      await createSuccessTest(data);
+      let uuid = await createSuccessTest(data);
+
+      // Now test that the related template was also created separately
+      let response = await request(app)
+      .get(`/template/${uuid}/draft`)
+      .set('Accept', 'application/json');
+      let related_template_uuid = response.body.related_templates[0].uuid;
+      response = await request(app)
+      .get(`/template/${related_template_uuid}/draft`)
+      .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(related_template_data);
     });
   
     test("Create template with related templates going 6 nodes deep", async () => {
@@ -219,7 +230,7 @@ describe("create / get draft", () => {
   
 });
 
-describe("update / get draft", () => {
+describe("update (and get draft after an update)", () => {
 
   let uuid;
 
@@ -369,4 +380,308 @@ describe("update / get draft", () => {
     })
   })
   
+});
+
+describe("publish (and get published and draft after a publish)", () => {
+
+  describe("Success cases", () => {
+
+    test("Simple publish - no fields and no related templates", async () => {
+
+      let data = {
+        "name":"basic template",
+        "description":"a template to test a publish",
+        "fields":[],
+        "related_templates":[]
+      };
+      let uuid = await createSuccessTest(data);
+
+      let response = await request(app)
+        .post(`/template/${uuid}/publish`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+    
+      // Check that a published version now exists
+      response = await request(app)
+        .get(`/template/${uuid}/latest_published`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(data);
+      expect(response.body).toHaveProperty("publish_date");
+
+
+      // Check that we can still get a draft version
+      response = await request(app)
+        .get(`/template/${uuid}/draft`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(data);
+    });
+
+    // TODO: when field endpoints are added, test they are also published
+    test("Complex publish - with nested fields and related templates to publish", async () => {
+
+      let related_template_data = {
+        "name": "a child template",
+        "fields": [{
+          "name": "a child field"
+        }]
+      };
+      let data = {
+        "name":"basic template",
+        "description":"a template to test a publish",
+        "fields":[{
+          "name": "a field"
+        }],
+        "related_templates":[related_template_data]
+      };
+      let uuid = await createSuccessTest(data);
+
+      let response = await request(app)
+        .post(`/template/${uuid}/publish`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+    
+      // Check that a published version now exists
+      response = await request(app)
+        .get(`/template/${uuid}/latest_published`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(data);
+
+      // Check that the related template was also published
+      let related_template_uuid = response.body.related_templates[0].uuid;
+      response = await request(app)
+        .get(`/template/${related_template_uuid}/latest_published`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(related_template_data);
+
+    });
+
+    test("Complex publish - changes in a nested property result in publishing for all parent properties", async () => {
+
+      let data = {
+        "name":"1",
+        "related_templates":[{
+          "name": "2",
+          "related_templates":[{
+            "name": "3",
+            "related_templates":[{
+              "name": "4"
+            }]
+          }]
+        }]
+      };
+      // Create initial data
+      let uuid = await createSuccessTest(data);
+
+      // Publish
+      let response = await request(app)
+        .post(`/template/${uuid}/publish`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+
+      response = await request(app)
+        .get(`/template/${uuid}/latest_published`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(data);
+
+      data = response.body;
+
+      // Make a change in the third level of data
+      data.related_templates[0].related_templates[0].description = "3 has a new description";
+
+      // Update with change
+      response = await request(app)
+        .put(`/template/${uuid}`)
+        .send(data)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+
+      // Record the date before we publish a second time
+      let intermediate_publish_date = (new Date()).getTime();
+
+      // Publish again
+      response = await request(app)
+        .post(`/template/${uuid}/publish`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+      
+      response = await request(app)
+        .get(`/template/${uuid}/latest_published`)
+        .set('Accept', 'application/json');
+      data = response.body;
+
+      // On the third node and above, the publish date should be newer than the intermediate_publish_date. 
+      // The fourth should be older
+      
+      expect(new Date(data.publish_date).getTime()).toBeGreaterThan(intermediate_publish_date);
+      expect(new Date(data.related_templates[0].publish_date).getTime()).toBeGreaterThan(intermediate_publish_date);
+      expect(new Date(data.related_templates[0].related_templates[0].publish_date).getTime()).toBeGreaterThan(intermediate_publish_date);
+      expect(new Date(data.related_templates[0].related_templates[0].related_templates[0].publish_date).getTime()).toBeLessThan(intermediate_publish_date);
+
+    });
+
+  })
+
+  describe("Failure cases", () => {
+    
+    test("Template with uuid does not exist", async () => {
+
+      let data = {
+        "name":"basic template"
+      };
+      await createSuccessTest(data);
+
+      let response = await request(app)
+        .post(`/template/${ValidUUID}/publish`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(404);
+
+    });
+
+    test("No changes to publish", async () => {
+      let data = {
+        "name":"basic template"
+      };
+      let uuid = await createSuccessTest(data);
+
+      let response = await request(app)
+        .post(`/template/${uuid}/publish`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(200);
+
+      response = await request(app)
+        .post(`/template/${uuid}/publish`)
+        .set('Accept', 'application/json');
+      expect(response.statusCode).toBe(400);
+    });
+
+  })
+
+
+});
+
+test("get published for a certain date", async () => {
+  let data = {
+    "name":"basic template",
+    "description": "1"
+  };
+  let uuid = await createSuccessTest(data);
+
+  let beforeFirstPublish = new Date();
+
+  let response = await request(app)
+    .post(`/template/${uuid}/publish`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  let afterFirstPublish = new Date();
+
+  data.uuid = uuid;
+  data.description = "2";
+
+  response = await request(app)
+    .put(`/template/${uuid}`)
+    .send(data)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  response = await request(app)
+    .post(`/template/${uuid}/publish`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  let afterSecondPublish = new Date();
+
+  data.description = "3";
+
+  response = await request(app)
+    .put(`/template/${uuid}`)
+    .send(data)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  response = await request(app)
+    .post(`/template/${uuid}/publish`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  // Now there should be three published versions. Search for each based on the date
+
+  response = await request(app)
+    .get(`/template/${uuid}/latest_published`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+  expect(response.body.description).toEqual(expect.stringMatching("3"));
+
+  response = await request(app)
+    .get(`/template/${uuid}/${(new Date()).toISOString()}`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+  expect(response.body.description).toEqual(expect.stringMatching("3"));
+
+  response = await request(app)
+    .get(`/template/${uuid}/${afterSecondPublish.toISOString()}`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+  expect(response.body.description).toEqual(expect.stringMatching("2"));
+
+  response = await request(app)
+    .get(`/template/${uuid}/${afterFirstPublish.toISOString()}`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+  expect(response.body.description).toEqual(expect.stringMatching("1"));
+
+  response = await request(app)
+    .get(`/template/${uuid}/${beforeFirstPublish.toISOString()}`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(404);
+});
+
+test("delete a draft, not a published version", async () => {
+  let data = {
+    "name":"basic template",
+    "description": "description"
+  };
+  let uuid = await createSuccessTest(data);
+
+  let response = await request(app)
+    .post(`/template/${uuid}/publish`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  data.uuid = uuid;
+  data.description = "different";
+
+  // Change the draft, but don't publish the change
+  response = await request(app)
+    .put(`/template/${uuid}`)
+    .send(data)
+  console.log(response.body);
+  expect(response.statusCode).toBe(200);
+
+  // Verify that the draft is what we changed it to
+  response = await request(app)
+    .get(`/template/${uuid}/draft`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  // Delete the draft
+  response = await request(app)
+    .delete(`/template/${uuid}/draft`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  // Get the draft again. Make sure it matches the latest published version
+  response = await request(app)
+    .get(`/template/${uuid}/latest_published`)
+    .set('Accept', 'application/json');
+  expect(response.statusCode).toBe(200);
+
+  data.description = "description";
+  expect(response.body).toMatchObject(data);
+
 });
