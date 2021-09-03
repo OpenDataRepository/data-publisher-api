17f4281ae928ccd645083c3675b1487b12915c40
Caleb Short <calebshort4697@gmail.com>
2021-09-03 12:15:31 -0400

    Tried to move the app initialization from individual tests suites to a single setup, but failed. Added documentation

--- test/globalSetup.js
+++ test/globalSetup.js
@@ -1,8 +1,15 @@
 var { MongoMemoryReplSet } = require('mongodb-memory-server');
+// var { init: appInit } = require('../app');
 
 module.exports = async function globalSetup() {
   let replset = await MongoMemoryReplSet.create({ replSet: { count: 1, storageEngine: 'wiredTiger'} });
   let uri = replset.getUri();
   process.env.DB = uri;
   global.replset = replset;
+
+  // TODO: the app should be initialized from here, the global setup.
+  // But for some reason I don't understand, if I initialize here, the mongoDB connection doesn't exist in the actual tests
+  // So eventually come back and figure this out.
+
+  // await appInit();
 }
--- test/globalTeardown.js
+++ test/globalTeardown.js
@@ -1,4 +1,8 @@
+// const MongoDB = require('../lib/mongoDB');
+
 module.exports = async function globalSetup() {
+  // await MongoDB.close();
+
   await global.replset.stop();
   delete global.replset;
 }
